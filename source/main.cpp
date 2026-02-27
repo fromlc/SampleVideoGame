fbfc838d97b674adbd1ec4e6f4325c7be8140f08
L Carver <wifihum@gmail.com>
2026-02-27 11:55:26 -0800

    Added file header links to SFML, Dear ImGui, ImGui-SFML repositories and licenses.

--- source/main.cpp
+++ source/main.cpp
@@ -7,6 +7,21 @@
 // This code is derived from Dr. Churchill's COMP 4300 Assignment 2,
 // presented on YouTube: https://youtu.be/-ugbLQlw_VM?si=tZnH4CMx3jueOFG5
 // See README file for details.
+// 
+// A big shout-out to all the devs contributing to SFML, Dear ImGui,
+// and ImGui-SFML! Thanks for being the heart of many cool projects.
+// 
+// SFML
+//		GitHub: https://github.com/SFML/SFML.git
+//		License: https://www.sfml-dev.org/license/
+// 
+// Dear ImGui
+//		GitHub: https://github.com/ocornut/imgui.git
+//		License: https://www.dearimgui.com/licenses/
+// 
+// ImGui-SFML
+//		GitHub: https://github.com/SFML/imgui-sfml.git
+//		License: https://github.com/SFML/imgui-sfml/blob/master/LICENSE
 //------------------------------------------------------------------------------
 #include "Game.h"
 
